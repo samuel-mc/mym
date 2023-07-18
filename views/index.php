d8b1cc3ba5c5dcd11d89f947fa2b21be51cb7efa
Samuel Medina <smdoomed@gmail.com>
2023-07-17 22:39:06 -0600

    Genericos

--- views/index.php
+++ views/index.php
@@ -8,8 +8,8 @@
                 <span class="font-weight-light" style="color: var(--yellow);">Unidad de Inspección</span>
             </h2>
         </div>
-        <div id="carouselHome" class="splide" aria-label="Splide Basic HTML Example">
-            <div class="splide__track">
+        <div id="carouselHome" class="splide w-100" aria-label="Splide Basic HTML Example">
+            <div class="splide__track" class="w-100">
                 <ul class="splide__list">
                     <li class="splide__slide">
                         <img src="<?php echo __ROOT__; ?>/public/img/home/carousel1.png" class="w-100">
@@ -17,7 +17,7 @@
                     <li class="splide__slide">
                         <div>
                             <h3 class="text--darkBlue">Acreditación y aprobación</h3>
-                            <p style="font-size: var(--text-lg);" class="text--darkBlue">U.V Marquez y Moncada está <span class="text--yellow">acreditada y aprobada</span> por la ema, la DGN respectivamente.</p>
+                            <p class="text--darkBlue text--md">U.V Marquez y Moncada está <span class="text--yellow">acreditada y aprobada</span> por la ema, la DGN respectivamente.</p>
                         </div>
                         <img src="<?php echo __ROOT__; ?>/public/img/home/carouselBg.png" class="w-100">
                     </li>
