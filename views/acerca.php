a4ec04d7fc6c4ba894d15a99331683609c9968ec
SebastianIslas <sebasislasbecerra@gmail.com>
2023-07-19 20:30:08 -0600

    Solicitud, acerca

--- views/acerca.php
+++ views/acerca.php
@@ -1,8 +1,101 @@
 <?php include 'partials/header.php'; ?>
 <?php include 'partials/navbar.php'; ?>
 
-<main class="">
+<main class="bg--image">
+    <section id="mision_vision" class="container">
+        <div class="grid--2cols text-center py-4 gap-8">
+            <div class="bg--mediumBlue text--white rounded-lg p-4">
+                <img class="rounded-lg img-fluid" src="<?php echo __ROOT__; ?>/public/img/acerca/mision.png">
+                <h2 class="my-4">Misión</h2>
+                <p>Coadyuvar con importadores y fabricantes nacionales, a dar cumplimiento a las Normas Oficiales Mexicanas de información comercial obligatoria, contribuyendo a fortalecer la normatividad en México.</p>
+            </div>
+            <div class="bg--mediumBlue text--white rounded-lg p-4">
+                <img class="rounded-lg img-fluid" src="<?php echo __ROOT__; ?>/public/img/acerca/mision.png">
+                <h2 class="my-4">Visión</h2>
+                <p>A través de servicios de calidad y compromiso del personal de UVMM, con el reconocimiento de las autoridades que nos acreditan y aprueban, y la satisfacción de los clientes por los servicios otorgados.</p>
+            </div>
+        </div>
+    </section>
 
+    <section id="beneficiosValor" class="container my-4">
+        <div class="text-center">
+            <h2 class="text--darkBlue font-weight-bold">Beneficios y valor agregado</h2>
+            <p class="text--gray">
+                Descubre nuestras ventajas que ayudaran a fortalecer las operaciones comerciales y  a reducir recursos de tu empresa.
+            </p>
+        </div>
+        <div class="grid--2cols">
+            <ul class="m-4 list-unstyled gap-2">
+                <li>
+                    <img class="rounded-lg img-fluid pr-4" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Eficiencia operativa</spam>
+                </li>
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Optimización de costos y tiempo en tus operaciones</spam>
+                </li>
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Garantizar el cumplimiento normativo</spam>
+                </li>
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Cobertura nacional en las inspecciones</spam>
+                </li>
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Capacitación constante de los inspectores</spam>
+                </li>
+            </ul>
+            <ul class="m-4 list-unstyled gap-2">
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Eficiencia operativa</spam>
+                </li>
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Amplia experiencia en el mercado</spam>
+                </li>
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Adaptabilidad a las necesidades de los clientes</spam>
+                </li>
+                <li>
+                    <img class="rounded-lg img-fluid pr-2" src="<?php echo __ROOT__; ?>/public/img/acerca/paloma.png">
+                    <spam>Atención y orientación personalizada</spam>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section id="nuestrosClientes" class="container my-4">
+        <div class="text-center">
+            <h2 class="text--darkBlue font-weight-bold">Nuestros Clientes</h2>
+            <p class="text--gray">
+                Contamos con la preferencia de clientes sólidos, gracias a su confianza y hemos crecido en este periodo gracias a nuestra experiencia en el medio.
+            </p>
+        </div>
+        <div class="grid--4cols gap-2 text-center">
+            <div class="m-4">
+                <img class="rounded-lg py-2" src="<?php echo __ROOT__; ?>/public/img/acerca/importadores.png">
+                <h3 class="text--darkBlue font-weight-bold">Importadores</h3>
+            </div>
+            <div class="m-4">
+                <img class="rounded-lg py-2" src="<?php echo __ROOT__; ?>/public/img/acerca/agentes.png">
+                <h3 class="text--darkBlue font-weight-bold">Agentes aduanales</h3>
+            </div>
+            <div class="m-4">
+                <img class="rounded-lg py-2" src="<?php echo __ROOT__; ?>/public/img/acerca/comercializadoras.png">
+                <h3 class="text--darkBlue font-weight-bold">Comercializadoras</h3>
+            </div>
+            <div class="m-4">
+                <img class="rounded-lg py-2" src="<?php echo __ROOT__; ?>/public/img/acerca/fabricantes.png">
+                <h3 class="text--darkBlue font-weight-bold">Fabricantes</h3>
+            </div>
+        </div>
+    </section>
+
+    <?php include 'components/bannerContacto.php'; ?>
 </main>
 
 <?php include 'partials/footer.php'; ?>
--- views/solicitud.php
+++ views/solicitud.php
@@ -2,6 +2,59 @@
 <?php include 'partials/navbar.php'; ?>
 
 <main class="">
+    <section class="container">
+        <div class="grid--2cols text-center py-4">
+            <div class="bg--mediumBlue text--white rounded-lg p-2">
+                <h2>Recuerda</h2>
+                <p>Para el despacho aduanal necesitas:</p>
+                <p class="text--sm">Contrato<br>
+                    SSI (valido)<br>
+                    Carta al administrador de aduana
+                </p>
+            </div>
+            <div class="p-2">
+                <h1 class="font-weight-bold text--darkBlue">Iniciar solicitud</h1>
+                <p class="text--gray">Unidad de inspección</p>
+                <a href="#" class="btn btn--yellow px-5 py-3 m-2">
+                    Requisitos <i class="fa-solid fa-arrow-right mx-1"></i>
+                </a>
+            </div>
+        </div>
+        <div class="text-center py-4">
+            <h1 class="font-weight-bold text--darkBlue">Descargar los siguientes documentos</h1>
+            <div class="grid--3cols">
+                <a href="#" class="btn btn--yellow px-5 py-3 m-2">
+                    Contrato <i class="fa-solid fa-arrow-right mx-1"></i>
+                </a>
+                <a href="#" class="btn btn--yellow px-5 py-3 m-2">
+                    SSI <i class="fa-solid fa-arrow-right mx-1"></i>
+                </a>
+                <a href="#" class="btn btn--yellow px-5 py-3 m-2">
+                    Contrato <i class="fa-solid fa-arrow-right mx-1"></i>
+                </a>
+            </div>
+        </div>
+        <div class="text-center py-4">
+            <h1 class="font-weight-bold text--darkBlue">Envianos los documentos al siguiente correo</h1>
+            <div class="text-center mx-0 px-0 mx-lg-5 px-lg-5">
+                <p class="bg--mediumBlue px-5 py-3 my-2 mx-0 w-full text--white rounded-lg">
+                    alejandraae@marquezymoncada.com
+                </p>
+            </div>
+            <div>
+                <a href="#" class="btn btn--yellow px-5 py-3 m-2">
+                    Carta dirigida al administrador de la aduana <i class="fa-solid fa-arrow-right mx-1"></i>
+                </a>
+            </div>
+            <h2 class="text--gray">(Enviar a tu agente aduanal)</h2>
+            <img class="rounded-lg img-fluid" src="<?php echo __ROOT__; ?>/public/img/general/video.png">
+            <div class="text-center mx-2 px-2 mx-lg-5 px-lg-5">
+                <h2 class="bg--mediumBlue px-lg-5 px-2 py-3 my-2 mx-lg-5  mx-2 text--white rounded-lg">
+                ¡Tu validación lista en 24 horas o menos!
+                </h2> 
+            </div>
+        </div>
+    </section>
 
 </main>
 
